d84e3fca09683f5bd0161a13d786c0ae57c8d9d2
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2022-01-30 13:36:08 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1,1,3,5,7,9,11,13,15,17,19,20};
+        int min = array[0];
+        System.out.println("Min values in array:");
+        for(int num : array){
+            if(num<min)
+            {
+                min=num;
+            }
+        }
+        for (int j = 0; j< array.length; j++) {
+            if (min == array[j])
+                System.out.print(array[j] + ", ");
+        }
+    }
+}
